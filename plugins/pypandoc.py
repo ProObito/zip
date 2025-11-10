9dad9936cf33500f222753f1b0b4e453129677b0
ProObito <ahmedumaid707@gmail.com>
2025-11-11 00:09:55 +0530

    Update pypandoc.py

--- plugins/pypandoc.py
+++ plugins/pypandoc.py
@@ -1,12 +1,1 @@
-import pypandoc
-import os
 
-def ensure_pandoc():
-    try:
-        pypandoc.get_pandoc_version()
-    except OSError:
-        print("[INFO] Pandoc not found. Downloading...")
-        pypandoc.download_pandoc()
-        print("[INFO] Pandoc installed successfully!")
-
-ensure_pandoc()
